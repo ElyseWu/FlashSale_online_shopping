7d82e9b6df0506abae859d8c640c20f7613f7c81
ElyseWu <huihwu@gmail.com>
2023-10-09 08:28:19 -0700

    add limit feature

--- src/main/java/com/jiuzhang/seckill/service/SeckillActivityService.java
+++ src/main/java/com/jiuzhang/seckill/service/SeckillActivityService.java
@@ -1,15 +1,19 @@
 package com.jiuzhang.seckill.service;
 
 import com.alibaba.fastjson.JSON;
+import com.jiuzhang.seckill.db.dao.OrderDao;
 import com.jiuzhang.seckill.db.dao.SeckillActivityDao;
 import com.jiuzhang.seckill.db.po.Order;
 import com.jiuzhang.seckill.db.po.SeckillActivity;
 import com.jiuzhang.seckill.mq.RocketMQService;
 import com.jiuzhang.seckill.util.SnowFlake;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.Date;
 
+@Slf4j
 @Service
 public class SeckillActivityService {
 
@@ -21,6 +25,9 @@ public class SeckillActivityService {
 
     @Resource
     private RocketMQService rocketMQService;
+
+    @Resource
+    private OrderDao orderDao;
 
     private SnowFlake snowFlake = new SnowFlake(1, 1);
 
@@ -52,6 +59,54 @@ public class SeckillActivityService {
          *2.发送创建订单消息
          */
         rocketMQService.sendMessage("seckill_order", JSON.toJSONString(order));
+        /*
+        * 3.发送订单付款状态校验消息
+        * 开源RocketMQ支持延迟消息，但是不支持秒级精度。默认支持18个level的延迟消息，这是通
+        过broker端的messageDelayLevel配置项确定的，如下：
+        * messageDelayLevel=1s 5s 10s 30s 1m 2m 3m 4m 5m 6m 7m 8m 9m 10m 20m
+        30m 1h 2h
+        */
+        rocketMQService.sendDelayMessage("pay_check", JSON.toJSONString(order),3);
         return order;
+    }
+
+
+    public void payOrderProcess(String orderNo) throws Exception {
+//        log.info("完成支付订单 订单号：" + orderNo);
+//        Order order = orderDao.queryOrder(orderNo);
+//        boolean deductStockResult =
+//                seckillActivityDao.deductStock(order.getSeckillActivityId());
+//        if (deductStockResult) {
+//            order.setPayTime(new Date());
+//            // 订单状态 0、没有可用库存，无效订单 1、已创建等待支付 2、完成支付
+//            //0: out of stock, 1: waiting for pay  2: paid
+//            order.setOrderStatus(2);
+//            orderDao.updateOrder(order);
+//        }
+        log.info("完成支付订单 订单号：" + orderNo);
+        Order order = orderDao.queryOrder(orderNo);
+        /*
+         * 1. check if order exist
+         * 2. check if order is valid
+         */
+        if (order == null) {
+            log.error("订单号对应订单不存在：" + orderNo);
+            return;
+        } else if(order.getOrderStatus() != 1 ) {
+            log.error("订单状态无效：" + orderNo);
+            return;
+        }
+        /*
+         * 2.order is paid
+         */
+        order.setPayTime(new Date());
+        //订单状态 0:没有可用库存，无效订单 1:已创建等待付款 ,2:支付完成
+        order.setOrderStatus(2);
+        orderDao.updateOrder(order);
+        /*
+         *3.send  order paid message
+         */
+        rocketMQService.sendMessage("pay_done", JSON.toJSONString(order));
+
     }
 }
